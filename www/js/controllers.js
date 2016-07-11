f0ee8d251fc6e8ecd0488bddcb4792500ecf97e2
babucse1991 <babucse1991@gmail.com>
2016-07-11 23:38:09 +0530

    firebase set up

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,28 @@
 angular.module('app.controllers', [])
 
-.controller('BrandSolutionsCtrl', function($scope) {
+.controller('BrandSolutionsCtrl', function($scope, sampleItems) {
+	
+	$scope.sampleItems = sampleItems;
+	
+	$scope.sampleData = {};
+	
+	$scope.saveSampleData =  function () {
+		$scope.sampleItems.$add($scope.sampleData); // data saved to firebase
+		console.log(" data saved.");
+		$scope.getSampleData();
+	}
+    
+	$scope.getSampleData =  function () {
+		
+		$scope.resultArray = [];
+		$scope.sampleItems.$getIndex().forEach(function (index) {
+			var eachItem = $scope.sampleItems[index];
+			console.log("eachItem : " + JSON.stringify(eachItem));
+			$scope.resultArray.push(eachItem);
+		});	
+	};
+	
+	
+	
 	
 });
